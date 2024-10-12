4f328e2d3df91d7e28605a5439d6026da5dce4b5
legend80s <liuchzong@qq.com>
2024-10-12 17:25:56 +0800

    feat: complete chapter#14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,30 +7,59 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['paid', 'pending']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than $0',
+  }),
+  status: z.enum(['paid', 'pending'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const rawFormData = CreateInvoice.parse(
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+
+  message?: string | null;
+};
+
+export async function createInvoice(_prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse(
     Object.fromEntries(formData.entries())
   );
 
-  console.log('rawFormData:', rawFormData);
+  console.log('rawFormData:', validatedFields.error);
 
-  const { customerId, amount, status } = rawFormData;
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch {
+    return {
+      message: 'Database Error. Failed to Create Invoice.',
+    };
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
